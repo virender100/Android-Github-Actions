4aa8ce9a117b3b8f3d3a5998505496e2fbdcd6bb
virender100 <virendersingh15012001@gmail.com>
2025-05-23 21:33:49 +0530

    Update ImagineApp.kt

--- app/src/main/java/com/wajahatkarim3/imagine/ImagineApp.kt
+++ app/src/main/java/com/wajahatkarim3/imagine/ImagineApp.kt
@@ -1,44 +1,16 @@
-package com.wajahatkarim3.imagine
+package com.github.wajahatkarim3.androidgithubactions
 
-import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
-import com.wajahatkarim3.imagine.di.components.DaggerAppComponent
-import com.wajahatkarim3.imagine.utils.isNight
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasAndroidInjector
-import javax.inject.Inject
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class ImagineApp: Application(), HasAndroidInjector {
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    @Inject
-    lateinit var androidInjector: DispatchingAndroidInjector<Any>
-
-    override fun onCreate() {
-        super.onCreate()
-        initDi()
-        setupDayNightMode()
-    }
-
-    fun initDi() {
-        DaggerAppComponent.builder()
-            .create(this)
-            .build()
-            .inject(this)
-    }
-
-    override fun androidInjector(): AndroidInjector<Any> {
-        return androidInjector
-    }
-
-    fun setupDayNightMode() {
-        // Get UI mode and set
-        val mode = if (isNight()) {
-            AppCompatDelegate.MODE_NIGHT_YES
-        } else {
-            AppCompatDelegate.MODE_NIGHT_NO
-        }
-
-        AppCompatDelegate.setDefaultNightMode(mode)
+        val textView = TextView(this)
+        textView.text = "Hello World"
+        textView.textSize = 32f
+        setContentView(textView)
     }
 }
